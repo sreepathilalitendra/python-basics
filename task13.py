f1590de880946f69e2e0e088258c068b8946130d
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-07 17:35:51 +0530

    Task is added successfully

---
+++ task13.py
@@ -0,0 +1,31 @@
+# largest number 
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+c = int(input("Enter the value of c:"))
+if a >= b and a >= c:
+    print(a," is a largest number")
+elif b >= a and b >=c:
+    print(b," is a largest number")
+else:
+    print(c," is a largest number")
+# even or Odd and positive and Negative 
+a = int(input("Enter a number:"))
+if a % 2 == 0 and a > 0:
+    print("Even and Positive number")
+elif a % 2 == 0 and a < 0:
+    print("Even and Negative number")
+elif a % 2!= 0 and a > 0:
+    print("Odd and Positive number")
+else: 
+    print("Odd and Negative number")
+# calculator program 
+a = int(input("Enter the value of a:"))
+a = 10
+b = 20
+if a > b :
+    print("A")
+elif b > a:
+    print("B")
+else:
+    print("Equal") # output B        
+
---
+++ task14.py
@@ -0,0 +1,36 @@
+# with out loop
+print("Hello world")
+print("Hello world")
+print("Hello world")
+# with loop
+for i in range(5):
+    print("Hello world")
+#for loop 
+for i in range (1,6):
+    print(i)
+#Example two 
+for i in range(5):
+    print(i)
+#Example -3  
+# even numbers using loop
+for i in range(2,11,2):
+    print(i)
+# Task 
+#print number from 1 to 10
+for i in range(1,11):
+    print(i)
+#print even numbers from 2 to 20 
+for i in range(2,21,2):
+    print(i)
+# print your name 5 time 
+for i in  range(5):
+    print("Lalitendra")
+# tables 
+for i in range (1,11):
+    print("5 x",i,"=",5*i)
+#Example two 
+n =int(input("Enter a number:"))
+for i in range(1,11):
+    print(n,"X",i,"=",n*i)
+    
+
