5338635f92fb3f44f4e0e1a5182b0f8ffabf68e3
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-07 17:59:46 +0530

    Task is added successfully

---
+++ task15.py
@@ -0,0 +1,29 @@
+# while loop
+#print numbers from 1 to 5 
+count = 1
+while count <=5:
+    print(count)
+    count +=1
+# printing number from 1 to 10
+i = 1
+while i <=10:
+    print(i)
+    i +=1
+#printing even number usinf while loop 
+num = 2
+while num <= 20:
+    print(num)
+    num += 2
+# printing name 5 times 
+name = 1
+while name <= 5:
+    print("Lalitendra")
+    name += 1
+#tables
+n = int(input("Enter a number:"))
+i = 1
+while i <= 10:
+    print(n,"X",i,"=", n*i)
+    i += 1
+
+    
