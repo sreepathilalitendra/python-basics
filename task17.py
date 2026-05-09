6b446f30becb95fef058d349bf53de16ec5c8a23
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-09 08:59:33 +0530

    Task is added Successfully

---
+++ task17.py
@@ -0,0 +1,52 @@
+# without list
+a = 10
+b = 20
+c = 30
+print(a)
+print(b)
+print(c)
+#with list 
+numbers = [10,20,30]
+print(numbers)
+# program 1 - print list
+numbers = [1,2,3,4]
+print(numbers)
+#program 2 - indexing 
+fruits =["apple","banana","mango"]
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+# program -3 changing values 
+num =[10,20,30]
+print(num) # before changing 
+num[1] = 99
+print(num) # After changing 
+# program 4 
+colors =["red" ,"yellow","green","black","Brown","blue"]
+print(colors)
+# program -5 
+value = [1,2,3,4]
+print(value) # before adding item 
+value.append(5)
+print(value)
+# Task 
+# 1 create a list of 5 numbers 
+numbers = [1,2,3,4,5]
+print(numbers)
+# print first and last items 
+print(numbers[0])
+print(numbers[-1])
+# change one value 
+numbers [3] = 99
+print(numbers)
+# add new values using append()
+numbers.append(10)
+print(numbers)
+# print list length 
+print(len(numbers))
+#challenge 
+names = ["Lalitendra","kiran","Rahul"] # list with names
+print(names[0]) # first name 
+print(names[-1]) # last name
+print(names) # total name 
