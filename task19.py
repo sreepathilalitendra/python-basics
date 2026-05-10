9ad99e4782ebc50acad2c30a4cd14a8f0ad515c6
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-10 06:47:42 +0530

    Task is added Successfully

---
+++ task19.py
@@ -0,0 +1,63 @@
+# tuple
+numbers = (1,2,3)
+print(numbers)
+print(type(numbers))
+# program 1
+data =(10,20,30)
+print(data)
+print(data[0])
+print(data[-1])
+#program-2
+colors = ("red","blue","green")
+print(len(colors))
+#dictionary
+#program -3
+student = {
+    "Name":"Lalitendra",
+    "Age":18 ,
+    "Branch":"CSE"
+}
+print(student)
+print(type(student))
+print(len(student))
+#Access values 
+print(student["Name"])
+print(student["Age"])
+print(student)
+#changing the values
+student ["Age"] = 19
+print(student)
+#add new value
+student ["college"] ="XYZ"
+print(student)
+#sets 
+#program -4 
+numbers = {1,2,3,4}
+print(numbers)
+#task 
+num =(1,2,3,4,5)
+print(num,type(num))
+print(num[0])
+print(num[-1])
+print(len(num))
+#dictionary
+student = {
+    "Name":"Lalitendra",
+    "Age":18,
+}
+print(student)
+print(type(student))
+print(student["Name"])
+student ["Age"] = 19
+print(student)
+student ["city"] = "Hyderabad"
+print(student)
+#set
+number ={1,2,3,4,5,6,2,4,6,1,3,2,4,5}
+print(number)
+#challenge
+favorite ={
+    "colors":"black",
+    "food" : "Biryani"
+}
+print(favorite)
