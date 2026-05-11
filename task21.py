3c3c7e059dd83bbd8903f143407ab062629872b6
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-11 08:55:57 +0530

    task is added successfully

--- task21.py
+++ task21.py
@@ -1,3 +1,27 @@
 # functions 
 def greet():
     print("Hello")
+#program 1
+def greet():
+    print("Welcome to python")
+greet()
+#program -2 
+def greet(name):
+    print("Hello",name)
+greet("Lalitendra")
+# program -3
+def add(a,b):
+    print("sum =",a+b)
+add(10,20)
+# program -4 
+def square(n):
+    return n * n
+result = square(5)
+print(result)
+def check(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+print(check(10))
+
