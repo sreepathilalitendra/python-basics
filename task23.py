bd41b22d71e51d2da8c44355f92251aa7d634e77
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-05-12 06:14:24 +0530

    Task is added Successfully

---
+++ task23.py
@@ -0,0 +1,16 @@
+# import math module 
+import math 
+print(math.sqrt(25))
+#program - 2
+import math
+print(math.pow(4,5))
+#program - 3 
+import random 
+print(random.randint(1,10))
+# program - 4
+import random 
+colors = ["red","blue","green",'orange','pink']
+print(random.choice(colors))
+# program -5
+import math as m 
+print(m.sqrt(45))
