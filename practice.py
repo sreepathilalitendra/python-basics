c6b63553b91cfd80cf5e3b922dbcc52fa8cf97a9
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-06-03 04:09:35 +0530

    Task is added Successfully

---
+++ practice.py
@@ -0,0 +1,80 @@
+# Level 1 
+# 1 question 
+naa_peru ="Lalitendra"
+print(naa_peru)
+favorite_city_name = "Hyderabad"
+print(favorite_city_name)
+print("Lalitendra")
+print("Hyderabad")
+name = input("Enter your name:")
+print("Your name is: ",name)
+# 2 question 
+a = 45
+b = 5
+print(a+b) #50
+print(a-b) #40
+print(45+5) # 50
+print(45-5) # 40 
+a = int(input("Enter the value of a:")) # 45
+b = int(input("Enter the value of b:")) # 5
+print(a+b) # 50
+print(a-b) # 40 
+# 3 question 
+result = 20 + 5 * 2 
+print(result) # output 5*2 = 10 ,20 + 10 = 30
+# level 2
+# 4 question 
+age = input("Enter your age:")
+age = int(age)
+print("present age:",age)
+print("Next year age :",age + 1)
+# 5 question 
+my_integer = int(input("Enter any integer value:"))
+my_float =  float(input("Enter any float value:"))
+total = my_integer + my_float
+print(total)
+# level 3
+# 6 question
+word = "programmming"
+first_character = word[0]
+last_character = word[-1]
+print("First character:", first_character)
+print("Last character:", last_character)
+# 7 question 
+name = input("Enter your name:") 
+print(name[0:3])
+# 8 question 
+word = "collage"
+print(word[::-1])
+# level - 4 
+# question 9
+num = int(input("Enter the value of num:"))
+if num > 0:
+    print(num,"is a positive number")
+else:
+    print(num,"is a negative number")
+# question 10 
+marks = int(input("Enter your marks:"))
+if marks >=35:
+    print("pass")
+else:
+    print("Fail bro ! Better lucky next time ")
+# question 11
+for i in range(7):
+    print("Lalitendra")
+# question 12 
+count = 1
+while count  <= 5:
+    print(count)
+    count +=1
+# question 13 
+color = ['Red ','Green','orange']
+color.append('Blue')
+print(color)
+my_dict ={
+    "car":"BMW"
+}
+print(my_dict)
+
+
+
