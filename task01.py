28cabd193431cc7d6b071cf510d9586f5975ee3e
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-02 11:13:43 +0530

    Task ia added Sucessfully

--- task01.py
+++ task01.py
@@ -2,8 +2,10 @@
 print("Lalitendra")
 name = "Lalitendra"
 print(name)
+# taking input from user 
 name = input("Enter your name:")
 print("Your name is",name)
+# calculations 
 #add two numbers 
 print(4+8)
 a = 2
@@ -29,10 +31,15 @@ X = int(input("Enter the values of X:"))
 Y = int(input("Enter the value of Y:"))
 print(X * Y)
 # division of two numbers
-print(20/5)
+print(20/5) # normal division
+print(20 // 5) # floor division 
 a = 24 
 b = 6 
 print(a / b )
+print(24//6)
 a = int(input("Enter the value of a:"))
 b = int(input("Enter the value of b:")) 
-print(a / b)    
+print(a / b)  
+print(a // b)  
+
+
