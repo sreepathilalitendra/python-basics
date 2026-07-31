30cdd40b89657f948a0624ec98388a31215edc1f
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 19:58:35 +0530

    Add while loop practice

---
+++ 11_while_loops.py
@@ -0,0 +1,42 @@
+# While loop
+
+# Print numbers from 1 to 5
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+
+# Print numbers from 1 to 10
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+
+# Print even numbers using while loop
+num = 2
+
+while num <= 20:
+    print(num)
+    num += 2
+
+
+# Print name 5 times
+name = 1
+
+while name <= 5:
+    print("Lalitendra")
+    name += 1
+
+
+# Tables
+n = int(input("Enter a number: "))
+
+i = 1
+
+while i <= 10:
+    print(n, "x", i, "=", n * i)
+    i += 1
