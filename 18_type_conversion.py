93155b649dbf38668f21d6cc1eca650d6c99de60
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 20:18:35 +0530

    Add type conversion practice

--- 18_type_conversion.py
+++ 18_type_conversion.py
@@ -4,9 +4,11 @@ a = input("Enter a number: ")
 a = int(a)
 print(a, type(a))
 
+
 b = input("Enter a decimal number: ")
 b = float(b)
 print(b, type(b))
+
 
 c = input("Enter the value of c: ")
 c = int(c)
@@ -14,8 +16,10 @@ c = int(c)
 d = 6
 print(c + d)
 
+
 a = 56
 print("Number is: " + str(a))
+
 
 a = input("Enter a number: ")
 b = input("Enter a number: ")
@@ -25,11 +29,13 @@ print("Before conversion:", type(a))
 total = int(a) + int(b)
 print("Sum =", total)
 
+
 a = input("Enter the value of a: ")
 print("Before conversion:", type(a))
 
 a = int(a)
 print("After conversion:", type(a))
+
 
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
