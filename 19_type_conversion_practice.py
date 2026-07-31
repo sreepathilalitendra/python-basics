7dc8cdbdd1ee3314553e47042a540dd3c4539a39
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 20:20:26 +0530

    Add type conversion practice programs

--- 19_type_conversion_practice.py
+++ 19_type_conversion_practice.py
@@ -4,9 +4,12 @@ b = float(input("Enter the value of b: "))
 
 print("Total =", a + b)
 
+
 # Take a number and double it
 a = int(input("Enter the value of a: "))
+
 print("Double =", a * 2)
+
 
 # Take three numbers and print their average
 a = int(input("Enter the value of a: "))
@@ -14,7 +17,9 @@ b = int(input("Enter the value of b: "))
 c = int(input("Enter the value of c: "))
 
 average = (a + b + c) / 3
+
 print("Average =", average)
+
 
 # Find the square and cube of a number
 a = int(input("Enter the value of a: "))
@@ -24,6 +29,7 @@ cube = a ** 3
 
 print("Square =", square)
 print("Cube =", cube)
+
 
 # Age type conversion
 age = input("Enter your age: ")
@@ -35,6 +41,7 @@ age = int(age)
 print("After conversion:", type(age))
 print("Next year age =", age + 1)
 
+
 # Sum, product, and average of two numbers
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
@@ -43,4 +50,5 @@ print("Sum =", a + b)
 print("Product =", a * b)
 
 average = (a + b) / 2
+
 print("Average =", average)
