6d77471d004b1f20676f0e0bb7192b7fbfb5c2ff
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 20:03:07 +0530

    Add lists and list operations practice

---
+++ 13_lists.py
@@ -0,0 +1,87 @@
+# Without list
+a = 10
+b = 20
+c = 30
+
+print(a)
+print(b)
+print(c)
+
+
+# With list
+numbers = [10, 20, 30]
+print(numbers)
+
+
+# Program 1 - Print list
+numbers = [1, 2, 3, 4]
+print(numbers)
+
+
+# Program 2 - Indexing
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+
+
+# Program 3 - Changing values
+num = [10, 20, 30]
+
+print(num)  # Before changing
+
+num[1] = 99
+
+print(num)  # After changing
+
+
+# Program 4 - List length
+colors = ["red", "yellow", "green", "black", "Brown", "blue"]
+
+print(colors)
+print(len(colors))
+
+
+# Program 5 - Adding values
+value = [1, 2, 3, 4]
+
+print(value)  # Before adding item
+
+value.append(5)
+
+print(value)  # After adding item
+
+
+# Task 1 - Create a list of 5 numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+
+# Print first and last items
+print(numbers[0])
+print(numbers[-1])
+
+
+# Change one value
+numbers[3] = 99
+print(numbers)
+
+
+# Add new value using append()
+numbers.append(10)
+print(numbers)
+
+
+# Print list length
+print(len(numbers))
+
+
+# Challenge
+names = ["Lalitendra", "Kiran", "Rahul"]
+
+print(names[0])   # First name
+print(names[-1])  # Last name
+print(names)      # All names
+print(len(names))
