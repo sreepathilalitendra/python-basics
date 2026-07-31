dc1c0ea14fe9f3b7f9d932c4e5ce59086588092e
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 19:45:35 +0530

    Add string operations practice

--- 08_string_operations.py
+++ 08_string_operations.py
@@ -1,35 +1,44 @@
-# join two strings
+# Join two strings
 a = "Hello"
 b = "World"
 print(a + " " + b)
+
 
 # String repetition
 my_str = " Lucky "
 print(my_str * 5)
 
-# indexing
+
+# Indexing
 name = "Python"
 print(name[0])
 
-# length of string
+
+# Length of string
 str1 = "Name"
 print(len(str1))
 
-# task
-# join a string
+
+# Task
+
+# Join a string
 a = "Sreepathi"
 b = "Lalitendra"
 print(a + " " + b)
 
-# repeat a string
+
+# Repeat a string
 str_1 = " Lalitendra "
 print(str_1 * 5)
 
-# print full name
+
+# Print full name
 first_name = "Lalitendra"
 last_name = "Sreepathi"
 print(first_name + " " + last_name)
 
+
+# User input
 name_1 = input("Enter your name: ")
 print("Hello", name_1)
 
