f205beb737f8eecf739933f3e7770b221b9f2de0
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 18:37:54 +0530

    Task is added Successfully

--- string_slicing.py
+++ string_slicing.py
@@ -1,20 +1,25 @@
-# string slicing 
+# string slicing
+
 name = "Python"
-print(name[0:3]) #pyt
-print(name[2:5])#tho
+print(name[0:3])  # pyt
+print(name[2:5])  # tho
+
 a = "Lalitendra"
 print(a[:5])
 print(a[5:])
+
 x = "Doctor"
 print(x[-3:])
-#Task 
-#1 take your name
+
+# Task
+# 1. Take your name
 c = "Lalitendra"
 print(c[0:3])
 print(c[-4:])
 print(c[4:6])
-#challenge
-k = input("Enter your name:")
+
+# Challenge
+k = input("Enter your name: ")
 print(k[0])
 print(k[-1])
 print(k[0:4])
