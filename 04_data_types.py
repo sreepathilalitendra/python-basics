6cfc2fe8ce37e08723a35fca986a1ee1bcda8397
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 19:24:00 +0530

    Add Python data types practice

--- 04_data_types.py
+++ 04_data_types.py
@@ -1,13 +1,15 @@
 # Data Types
-a = 2          # Integer
-b = 2.5        # Float
-c = "Lalitendra"  # String
-d = 3 + 2j     # Complex
+
+a = 2              # Integer
+b = 2.5            # Float
+c = "Lalitendra"   # String
+d = 3 + 2j         # Complex
 
 print(a, type(a))
 print(b, type(b))
 print(c, type(c))
 print(d, type(d))
+
 
 # Taking input for different data types
 a = int(input("Enter the value of a: "))
