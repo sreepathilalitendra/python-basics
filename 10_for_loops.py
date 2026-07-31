127de28ce541af4cef6c2ad5e3d8b6161e2aabf2
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 19:53:56 +0530

    Add for loop and range practice

---
+++ 10_for_loops.py
@@ -0,0 +1,51 @@
+# Without loop
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+
+# With loop
+for i in range(5):
+    print("Hello world")
+
+
+# For loop
+for i in range(1, 6):
+    print(i)
+
+
+# Example two
+for i in range(5):
+    print(i)
+
+
+# Even numbers using loop
+for i in range(2, 11, 2):
+    print(i)
+
+
+# Task - Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+
+# Print even numbers from 2 to 20
+for i in range(2, 21, 2):
+    print(i)
+
+
+# Print your name 5 times
+for i in range(5):
+    print("Lalitendra")
+
+
+# Tables
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
+
+
+# Example - User input table
+n = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(n, "x", i, "=", n * i)
