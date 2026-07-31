bd5c69e858cf1a2aa1870383d5cf1b65ebc2f7a8
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-31 20:09:45 +0530

    Add Python basics practice

--- 01_python_basics.py
+++ 01_python_basics.py
@@ -70,3 +70,37 @@ print(a % b)
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
 print(a % b)
+
+# Even or Odd
+n = int(input("Enter number: "))
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Reverse a string
+name = input("Enter name: ")
+print(name[::-1])
+
+
+# Sum of the list
+num = [10, 20, 30]
+total = 0
+
+for i in num:
+    total += i
+
+print(total)
+
+
+# Find the largest number in a list
+numbers = [4, 9, 2, 15, 7]
+largest = numbers[0]
+
+for i in numbers:
+    if i > largest:
+        largest = i
+
+print(largest)
